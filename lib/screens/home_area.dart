a7b8b71e893383a457330f9ed34b372dfb7a7bf9
Johnny <johnny.ayush@rubicotech.in>
2022-02-15 16:14:53 +0530

    Gesture detecture added

--- lib/screens/home_area.dart
+++ lib/screens/home_area.dart
@@ -1,6 +1,4 @@
-// import 'package:faith_music_player/widgets/play_music.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -12,114 +10,10 @@ class HomeArea extends StatefulWidget {
 }
 
 class _HomeAreaState extends State<HomeArea> {
-  bool valueFromGesture = false;
-  String song = "La Casa De Papel SoundTrack - My Life is going on";
-  String artist = "Cecilia Krull";
-  @override
-  Widget build(BuildContext context) {
-    var fixedHeight = MediaQuery.of(context).size.height * 0.10;
-    var fixedWidth = MediaQuery.of(context).size.width * 0.70;
+  //################################################
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 25.0,
-        ),
-        Center(
-          child: GestureDetector(
-            onTap: () {
-              // PlayButton().playPause(valueFromGesture);
-              // miniPlayerState.playPause;
-            },
-            child: Container(
-              width: fixedWidth,
-              height: fixedWidth,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                image: const DecorationImage(
-                  image: AssetImage('assets/images/logo.png'),
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 12.0,
-        ),
-        //Song Name and Artist Name
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: fixedHeight * 1.5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    sliceString(song),
-                    style: TextStyle(
-                      color: Colors.amber[50],
-                      fontWeight: FontWeight.w600,
-                      fontSize: 22,
-                    ),
-                  ),
-                  Text(
-                    artist,
-                    style: TextStyle(
-                      color: Colors.amber[50],
-                      fontWeight: FontWeight.w600,
-                      fontSize: 13,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-         const SizedBox(
-          height: 12.0,
-        ),
-         //////----------->
-         //////Player Area
-         //////----------->
-         const MiniPlayer(), 
-      ],
-    );
-  }
-
-//SETTING THE STRING LENGTH WITHIN 20 LETTERS
-  String sliceString(String word) {
-    String slicedString = '';
-    if (word.length > 25) {
-      for (int i = 0; i < 25; i++) {
-        slicedString = slicedString + (word[i]);
-      }
-      // print(slicedString);
-      return slicedString + "...";
-    } else {
-      // print(word);
-      return word;
-    }
-  }
-}
-
-
-//
-////MUSIC PLAYING AREA-------------------------------->
-//
-
-class MiniPlayer extends StatefulWidget {
-   const MiniPlayer({Key key}) : super(key: key);
-
-  @override
-  _MiniPlayerState createState() => _MiniPlayerState();
-}
-
-class _MiniPlayerState extends State<MiniPlayer> {
+// MUSIC PLAYER LOGIC STARTS-------->
+//----------------------------------------------->
   bool playing = false;
   IconData playBtn = Icons.play_arrow;
 
@@ -130,8 +24,7 @@ class _MiniPlayerState extends State<MiniPlayer> {
   Duration position = const Duration();
   Duration musicLength = const Duration();
 
-  // DEFAULT APPBAR
-  var scaffoldKey = GlobalKey<ScaffoldState>();
+  //################################################
 
   //Slider for music streaming
   Widget slider() {
@@ -148,13 +41,17 @@ class _MiniPlayerState extends State<MiniPlayer> {
       ),
     );
   }
-
+  //################################################??????????
 
 //Will set the state of slider when dragged---->>>>
   void seekToSec(int sec) {
     Duration newPos = Duration(seconds: sec);
     _player.seek(newPos);
   }
+
+  //################################################???????????
+
+////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
 //////////////////////----------MUSIC PLAYER FUNCTIONS STARTS
@@ -165,6 +62,8 @@ class _MiniPlayerState extends State<MiniPlayer> {
     _player = AudioPlayer();
     cache = AudioCache(fixedPlayer: _player);
 
+    //################################################{{{---INSIDE VOID INIT}}}
+
     //audio player time handling, will help in getting the music duration MDH MDH
     // ignore: deprecated_member_use
     _player.durationHandler = (d) {
@@ -172,7 +71,6 @@ class _MiniPlayerState extends State<MiniPlayer> {
         musicLength = d;
       });
     };
-
 
 //this will help in moving the cursor while playing the music, asli masale sach sach
     // ignore: deprecated_member_use
@@ -184,8 +82,11 @@ class _MiniPlayerState extends State<MiniPlayer> {
 
     cache.load("sound_test.mp3");
     //no need to specify the full path here, just put the music file in asset folder - best practice noted
+
+    //################################################{{{---INSIDE VOID INIT}}}
   }
 
+//################################################??????????
 //Will set the state of music button according to the play status---->>>>
   void playPause() {
     if (!playing) {
@@ -203,13 +104,21 @@ class _MiniPlayerState extends State<MiniPlayer> {
       });
     }
   }
-
+  //################################################??????????
 
 //////////////////////----------MUSIC PLAYER FUNCTIONS ENDS
 //////////////////////////////////////////////////////////////
+//----------------------------------------------->
+//MUSIC PLAYER LOGIC ENDS-------->
 
-  @override
-  Widget build(BuildContext context) {
+  String song = "La Casa De Papel SoundTrack - My Life is going on";
+  bool valueFromGesture = false;
+  String artist = "Cecilia Krull";
+
+  //MUSIC PLAYING AREA WIDGET STARTS-------->
+  //-------------------------------------------------------->
+
+  Widget miniPlayer(BuildContext context) {
     return Expanded(
       child: Container(
         height: (MediaQuery.of(context).size.height) * 0.20,
@@ -220,12 +129,11 @@ class _MiniPlayerState extends State<MiniPlayer> {
             topRight: Radius.circular(30.0),
           ),
         ),
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            //Slider and Start and End Duration of Music
+            //Slider and Start and End Duration of Music---------->
             SizedBox(
               width: 500.0,
               child: Row(
@@ -338,5 +246,102 @@ class _MiniPlayerState extends State<MiniPlayer> {
         ),
       ),
     );
+  }
+
+  //-------------------------------------------------------->
+  //MUSIC PLAYING AREA WIDGET ENDS-------->
+
+  @override
+  Widget build(BuildContext context) {
+    var fixedHeight = MediaQuery.of(context).size.height * 0.10;
+    var fixedWidth = MediaQuery.of(context).size.width * 0.70;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 25.0,
+        ),
+        Center(
+          child: GestureDetector(
+            onTap: () {
+              print("PRINT ONTAP");
+              playPause(); // PlayButton().playPause(valueFromGesture);
+              // miniPlayerState.playPause;
+            },
+            //
+            // MUSIC ALBUM IMAGE
+            child: Container(
+              width: fixedWidth,
+              height: fixedWidth,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        //SONG NAME AND ARTIST NAME
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: fixedHeight * 1.5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    sliceString(song),
+                    style: TextStyle(
+                      color: Colors.amber[50],
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    artist,
+                    style: TextStyle(
+                      color: Colors.amber[50],
+                      fontWeight: FontWeight.w600,
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        //////----------->
+        //////PLAYER AREA
+        //////----------->
+        miniPlayer(context),
+      ],
+    );
+  }
+
+//SETTING THE STRING LENGTH WITHIN 25 LETTERS
+  String sliceString(String word) {
+    String slicedString = '';
+    if (word.length > 25) {
+      for (int i = 0; i < 25; i++) {
+        slicedString = slicedString + (word[i]);
+      }
+      // print(slicedString);
+      return slicedString + "...";
+    } else {
+      // print(word);
+      return word;
+    }
   }
 }
